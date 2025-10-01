5bdcb8876b4e4d66ce493ee60af2296c954989d0
Jonathan <jonathanutsuni@gmail.com>
2025-09-30 21:43:16 -0300

    Criação do Dockerfile, tratamento de erros para mensagens do RabbitMQ e configurações de ambiente

---
+++ src/main/java/fiap/tech/challenge/hospital_manager/exception/handlers/RabbitMqErrorHandler.java
@@ -0,0 +1,28 @@
+package fiap.tech.challenge.hospital_manager.exception.handlers;
+
+import com.rabbitmq.client.Channel;
+import fiap.tech.challenge.hospital_manager.dto.in.ConsultaIn;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ErrorHandler;
+
+@Component
+@Slf4j
+public class RabbitMqErrorHandler implements ErrorHandler {
+
+    @Override
+    public void handleError(Throwable t) {
+        log.error("Erro ao processar mensagem RabbitMQ", t);
+    }
+
+    public void handleInvalidMessage(ConsultaIn consultaIn, Message message, Channel channel, Exception e) {
+        try {
+            log.warn("Mensagem inválida descartada: {} - Motivo: {}", new String(message.getBody()), e.getMessage());
+            // Rejeita a mensagem sem reencaminhar (evita loop infinito)
+            channel.basicReject(message.getMessageProperties().getDeliveryTag(), false);
+        } catch (Exception ex) {
+            log.error("Erro ao descartar mensagem do RabbitMQ", ex);
+        }
+    }
+}
